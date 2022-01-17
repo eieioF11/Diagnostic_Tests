23776c79a3c93be934f4f7ed7d7ff6832660688b
eieioF11 <57821360+eieioF11@users.noreply.github.com>
2022-01-17 09:33:30 +0900

    [fix]fixed text

--- cgi-bin/form_test.py
+++ cgi-bin/form_test.py
@@ -3,7 +3,7 @@ import sys
 import io
 import cgi
 from line_notify_bot import LINENotifyBot
-
+bot = LINENotifyBot(access_token='l0xllBCP6WC4QiI0HThc4zQKZFrDU22bDmiXTO0CVAM')
 form = cgi.FieldStorage()
 sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding="utf-8")
 print("Content-Type: text/html;charset=UTF-8")
@@ -14,7 +14,6 @@ text=""
 for key in form:
     value = form[key].value
     print("<p> %s: %s </p>" % (key, value))
-    text=stl(key)+":"+stl(value)
-bot = LINENotifyBot(access_token='l0xllBCP6WC4QiI0HThc4zQKZFrDU22bDmiXTO0CVAM')
+    text=str(key)+":"+str(value)
 bot.send(message=text)
 print("</body></html>")

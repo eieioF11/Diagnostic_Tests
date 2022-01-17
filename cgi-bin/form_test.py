ef1dd9111730c2d7cd6eecb1babb629c44978402
Nishizawa Eiji <eieioeiji0501@gmail.com>
2022-01-17 10:21:17 +0900

    [add]Return to Home function added

--- cgi-bin/form_test.py
+++ cgi-bin/form_test.py
@@ -12,7 +12,7 @@ form = cgi.FieldStorage()
 sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding="utf-8")
 print("Content-Type: text/html;charset=UTF-8")
 print()
-print("<html><head></head><body>")
+print("<html><head></head><body onLoad=setTimeout(\"location.href=\'../html/index.html\'\",500)>")
 print("<h1>Pythonにform</h1>")
 for key in form:
     value = form[key].value

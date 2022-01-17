6088e0a62c3fdf91aab3cbf01e1540227d87305b
eieioF11 <57821360+eieioF11@users.noreply.github.com>
2022-01-17 09:22:14 +0900

    add LINE Notify

--- cgi-bin/form_test.py
+++ cgi-bin/form_test.py
@@ -2,13 +2,20 @@
 import sys
 import io
 import cgi
+from line_notify_bot import LINENotifyBot
+
 form = cgi.FieldStorage()
 sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding="utf-8")
 print("Content-Type: text/html;charset=UTF-8")
 print()
 print("<html><head></head><body>")
 print("<h1>Pythonにform</h1>")
+text=""
 for key in form:
     value = form[key].value
     print("<p> %s: %s </p>" % (key, value))
+    text=stl(key)+":"+stl(value)
 print("</body></html>")
+
+bot = LINENotifyBot(access_token='l0xllBCP6WC4QiI0HThc4zQKZFrDU22bDmiXTO0CVAM')
+bot.send(message=text)
